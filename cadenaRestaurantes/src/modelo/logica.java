9b7f4650811e9ffe614cb2de23796c77d794852c
AlvaroSnts <alvaro22dj@gmail.com>
2022-10-18 11:09:33 +0200

    No se cambia nada

---
+++ cadenaRestaurantes/src/modelo/logica.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import controlador.coordinador;
+
+public class logica {
+	
+	private coordinador coordinador;
+	
+	public void setCoordinador(coordinador coordinador) {
+		this.coordinador=coordinador;
+	}
+}
