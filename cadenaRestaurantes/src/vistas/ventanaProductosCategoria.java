476279452cea8906d849a81d9c3d0e6e95f25bc9
AlvaroSnts <alvaro22dj@gmail.com>
2022-10-18 11:30:35 +0200

    Ventanas CarritoYProductosCategoria

--- cadenaRestaurantes/src/vistas/ventanaProductosCategoria.java
+++ cadenaRestaurantes/src/vistas/ventanaProductosCategoria.java
@@ -8,6 +8,9 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import controlador.coordinador;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class ventanaProductosCategoria extends JFrame implements ActionListener {
 
@@ -16,14 +19,19 @@ public class ventanaProductosCategoria extends JFrame implements ActionListener 
 	private JLabel labelTitulo;
 	private JButton botonVolver;
 	private coordinador coordinador;
+	private JButton Carrito;
 
 
 	public ventanaProductosCategoria() {
-		setSize(450, 400);
+		setSize(750, 520);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Categoria");
 		construirPanel();
 		setContentPane(panel);
+		
+		Carrito = new JButton("Carrito");
+		Carrito.setBounds(605, 419, 119, 51);
+		panel.add(Carrito);
 	    setLocationRelativeTo(null);
 		setVisible(false);
 	}
@@ -34,15 +42,18 @@ public class ventanaProductosCategoria extends JFrame implements ActionListener 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBackground(new Color(0, 0, 0));
 		panel.setLayout(null);
 		
 		labelTitulo = new JLabel("Productos de la categoria:");
-		labelTitulo.setBounds(152, 11, 172, 14);
+		labelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		labelTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		labelTitulo.setForeground(new Color(255, 255, 255));
+		labelTitulo.setBounds(152, 11, 431, 100);
 		panel.add(labelTitulo);
 		
 		botonVolver = new JButton("Volver");
-		botonVolver.setBounds(346, 227, 78, 23);
+		botonVolver.setBounds(10, 447, 78, 23);
 		panel.add(botonVolver);
 	}
 	
@@ -53,5 +64,4 @@ public class ventanaProductosCategoria extends JFrame implements ActionListener 
 	public void actionPerformed(ActionEvent e) {
 		
 	}
-
 }
