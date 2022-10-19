119df9433ea5b21105465fc7a37d92688191f63a
JaviMoraales <101357089+JaviMoraales@users.noreply.github.com>
2022-10-19 16:34:19 +0200

    logica actualizada

--- cadenaRestaurantes/src/modelo/logica.java
+++ cadenaRestaurantes/src/modelo/logica.java
@@ -1,6 +1,12 @@
 package modelo;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 import controlador.coordinador;
+import modelo.dao.categoriasDao;
+import modelo.vo.categoriasVo;
 
 public class logica {
 	
@@ -8,5 +14,19 @@ public class logica {
 	
 	public void setCoordinador(coordinador coordinador) {
 		this.coordinador=coordinador;
+	}
+	
+	public void validarMostrarTodasLasCategorias(){
+		categoriasDao categoria=new categoriasDao();
+		ArrayList<categoriasVo> categorias=new ArrayList<categoriasVo>();
+		
+		categorias=categoria.mostrarTodasLasCategorias(categorias);
+		
+		if (categorias == null) {
+			JOptionPane.showMessageDialog(null,"No hay clientes","Advertencia",JOptionPane.WARNING_MESSAGE);
+		}else {
+			coordinador.mostrarTodasLasCategorias(categorias);
+		}
+
 	}
 }
