b7213279f173da3de09dfd0da8166ab699485e83
JaviMoraales <101357089+JaviMoraales@users.noreply.github.com>
2022-10-21 23:06:05 +0200

    Add files via upload

--- cadenaRestaurantes/src/modelo/dao/categoriasDao.java
+++ cadenaRestaurantes/src/modelo/dao/categoriasDao.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
 import modelo.conexion.conexion;
 
 import modelo.vo.categoriasVo;
+import vistas.ventanaListaCategorias;
 
 public class categoriasDao {
 
@@ -41,4 +42,34 @@ public ArrayList<categoriasVo> mostrarTodasLasCategorias(ArrayList<categoriasVo>
 		}
 		else return null;				
 	}
+
+public ArrayList<categoriasVo> mostrarCategoriaPorNombre(ArrayList<categoriasVo> categorias){
+	
+	conexion conexionBD= new conexion();
+	
+	boolean existe=false;
+	try {
+		Statement estatuto = conexionBD.conectarBD().createStatement();
+		ResultSet res = estatuto.executeQuery("SELECT * FROM categorias WHERE nombre='"+ventanaListaCategorias.categoriaString+"';");
+		while(res.next()){
+			existe=true;
+			categoriasVo categoria= new categoriasVo();
+			categoria.setCodCat(res.getInt("codCat"));
+			categoria.setNombre(res.getString("nombre"));
+			categoria.setDescripcion(res.getString("descripcion"));
+			categorias.add(categoria);
+		 }
+		res.close();
+		conexionBD.desconectarBD();
+							
+	} catch (SQLException e) {
+		JOptionPane.showMessageDialog(null, "Error, no se ha podido conectar");
+		System.out.println(e);
+	}
+	
+	if (existe) {
+		return categorias;
+	}
+	else return null;				
+}
 }
--- cadenaRestaurantes/src/modelo/dao/productosDao.java
+++ cadenaRestaurantes/src/modelo/dao/productosDao.java
@@ -2,27 +2,50 @@ package modelo.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 import modelo.conexion.conexion;
+import modelo.vo.categoriasVo;
 import modelo.vo.productosVo;
+import vistas.ventanaListaCategorias;
 
 public class productosDao {
-	public static String mostrarTodosLosProductosCateCategoria() {
-		String productos = "";
-		conexion conexionBD = new conexion();
-		productosVo productosVo = new productosVo();
+
+	public ArrayList<productosVo> mostrarTodosLosProductosCategoria(ArrayList<productosVo> productos){
+		
+		conexion conexionBD= new conexion();
+		
+		boolean existe=false;
 		try {
-			PreparedStatement preparedStatement1 = conexionBD.conectarBD().prepareStatement("SELECT * FROM productos");
-			ResultSet res = preparedStatement1.executeQuery();
-			while(res.next()) {
-				if(productosVo.getCategoria()==1) {
-					productos=productos+"Nombre Producto: "+productosVo.getNombre();
-					System.out.println("Hola");
-				}
-			}
-		}catch(Exception e) {
+			Statement estatuto = conexionBD.conectarBD().createStatement();
+			ResultSet res = estatuto.executeQuery("SELECT * FROM productos WHERE categoria='"+ventanaListaCategorias.categoriaInt+"';");
+			System.out.println(ventanaListaCategorias.categoriaInt);
+			while(res.next()){
+				existe=true;
+				productosVo producto= new productosVo();
+				producto.setCodProd(res.getInt("codProd"));
+				producto.setNombre(res.getString("nombre"));
+				producto.setDescripcion(res.getString("descripcion"));
+				producto.setPeso(res.getDouble("peso"));
+				producto.setStock(res.getInt("stock"));
+				producto.setCategoria(res.getInt("categoria"));
+				productos.add(producto);
+			 }
+			res.close();
+			conexionBD.desconectarBD();
+								
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Error, no se ha podido conectar");
 			System.out.println(e);
-		};
-		return productos;
+		}
+		
+		if (existe) {
+			return productos;
+		}
+		else return null;				
 	}
 }
