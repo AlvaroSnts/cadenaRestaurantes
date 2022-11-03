8433857fdd491239bbcda37e04a19aced2c14d59
JaviMoraales <101357089+JaviMoraales@users.noreply.github.com>
2022-11-03 19:38:58 +0100

    conexion actualizao

--- cadenaRestaurantes/src/modelo/conexion/conexion.java
+++ cadenaRestaurantes/src/modelo/conexion/conexion.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 public class conexion {
 	   static String bd = "cadenaRestaurantes";
-	   private static String login;
-	   private static String password;
+	   private static String login="administrador";
+	   private static String password="administrador";
 	   static String url = "jdbc:mysql://localhost/"+bd;
 
 	   Connection conn = null;
